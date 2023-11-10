796e267f4f0de2b0eebd4ea300e30436e9659101
samyakmaitre <pixelpirate404@gmail.com>
2023-11-10 20:05:12 +0530

    skdvjb

---
+++ cg.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_certificates', methods=['POST'])
+def generate_certificates():
+    # Add backend logic to handle file uploads, generate certificates, and return the result.
+    # This could involve using a library like Openpyxl for Excel processing and Pillow for image manipulation.
+    # Return a JSON response with the result.
+    pass  # You need to replace this with your implementation
+
+if __name__ == '__main__':
+    app.run(debug=True)
