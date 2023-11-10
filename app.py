6b73a9bc22fe1afa2e6845a9c538b684467db83b
samyakmaitre <pixelpirate404@gmail.com>
2023-11-10 20:56:38 +0530

    lksdnc dsl

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from flask import Flask, render_template, request, send_file
-import csv
-from io import StringIO
-from PIL import Image, ImageDraw
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    # Handle image upload logic here
-    # Update selectedRectangle based on uploaded image dimensions
-    return jsonify({'status': 'success'})
-
-@app.route('/upload-csv', methods=['POST'])
-def upload_csv():
-    # Handle CSV upload logic here
-    # Update textOverlay based on uploaded CSV file
-    return jsonify({'status': 'success'})
-
-@app.route('/download-image', methods=['GET'])
-def download_image():
-    # Download image with text logic here
-    # Use PIL to draw text on the image based on the uploaded text
-    return send_file('path/to/downloaded_image.jpg', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
